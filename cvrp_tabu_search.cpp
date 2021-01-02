36be83e71efdb4a2c8aa53339727103e05b7c117
Robert Kumanek <robert.kumanek@gmail.com>
2021-01-02 12:20:18 +0100

    Add tsp_tabu_search

--- cvrp_tabu_search.cpp
+++ cvrp_tabu_search.cpp
@@ -1,116 +1,243 @@
 #include "cvrp_tabu_search.hpp"
 
-
 //funkcja do obliczania odległosci dla miast
-double distance_between_cities(Coordinate first_city, Coordinate second_city){
-    return sqrt(pow(first_city.length - second_city.length, 2) + pow(first_city.width- second_city.width, 2));
+double distance_between_cities( City& first_city, City& second_city){
+    return geo_degree_in_km * ( sqrt( pow(first_city.m_coordinate.length - second_city.m_coordinate.length, 2) +
+                                      pow(first_city.m_coordinate.width  - second_city.m_coordinate.width,  2)  ) );
 }
 
-void distance_for_two_cities_with_coordinates_from_pythagorean_triangle(){
-
-    const Coordinate Krakow{0, 3};
-    const Coordinate Bialystok{4, 0};
-    assert(distance_between_cities(Krakow,Bialystok) == 5);
-}
 // sprawdzenie czy liczba nalezy do zakresu (bo double nie sa dokładne wiec trzeba zakres)
-bool is_near_enough_to(double supect, double center, double measurement_error ){
-    return supect >= center - measurement_error && supect <= center + measurement_error;
+bool is_near_enough_to(double suspect, double center, double measurement_error ){
+    return suspect >= center - measurement_error && suspect <= center + measurement_error;
 }
 
-void is_near_enough_to_test(){
-    assert(is_near_enough_to(4,2,3));
-    assert(!is_near_enough_to(10,2,3));
-}
-
-//obliczanie dystanu dla 2 miast
-void distance_for_two_cities(){
-
-    const Coordinate Krakow{50.0619474, 19.9368564};
-    const Coordinate Bialystok{53.132398, 23.1591679};
-    assert(is_near_enough_to(distance_between_cities(Krakow,Bialystok), 4.45095) == true);
-}
-
-// sprawdzenie macierzy dla 2 miast
-
-void distance_matrix_for_two_cities(){
-
-    const Coordinate Krakow{50.0619474, 19.9368564};
-    const Coordinate Bialystok{53.132398, 23.1591679};
-
-    std::vector<std::vector<double>> expected_distance_matrix = {
-        {0, 4.45095},
-        {4.45095, 0}
-    };
-
-     std::vector<std::vector<double>> distance_matrix = {
-        {distance_between_cities(Krakow,Krakow), distance_between_cities(Krakow,Bialystok)},
-        {distance_between_cities(Bialystok,Krakow), distance_between_cities(Bialystok,Bialystok)},
-     };
-
-     for(int i = 0; i < distance_matrix.size(); i++){
-         for(int j = 0; j < distance_matrix.size(); j++){
-             assert(is_near_enough_to(distance_matrix[i][j], expected_distance_matrix[i][j]));
-         }
-     }
-}
-
-void make_real_distance_matrix(){
- 
-    const int length_patter = 11;
-    for( int i = 0; i < cities.size(); i++ )
-    {
-        std::cout << std::setw(length_patter) << name_of_cities[ i ] << " ";
+void display(Route route){
+    const int word_width = 15;
+    std::cout << "Whole distance is " << fitness(route) << std::endl;
+    for (int i = 0; i < route.size(); i++){
+        std::cout << std::setw( word_width ) <<  route[ i ].m_name << " ";
     }
     std::cout << std::endl;
-
-    for( int i = 0; i < cities.size(); i++ )
-    {
-        for( int j = 0; j < cities.size(); j++ )
-            std::cout << std::setw(length_patter) << geo_degree_in_km * distance_between_cities( cities[ i ], cities[ j ] ) << " ";
-        std::cout << std::endl;
-    }
 }
 
-std::tuple<std::vector<double>,
-           std::vector<double>,
-           std::vector<std::string>> our_cities()
-{
-    std::vector<double> x;
-    std::vector<double> y;
-    std::vector<std::string> name;
-    for( int i = 0; i < cities.size(); i++ )
-    {
-        x.push_back( cities[i].length );
-        y.push_back( cities[i].width );
-        name.push_back( name_of_cities[i] );
-    }
-    return std::make_tuple( x, y, name );
-}
-
-void draw_cities() 
+void draw_solution(std::vector<Route> solution, int color)
 {
     using namespace matplot;
-    auto [lon,lat,names] = our_cities();
-    // int i = 0;
-    // while( names[ ++i ] != "Warsaw" );
-    // std::cout << lon [ i ] << " " << lat [ i ] << " " << names [ i ] << std::endl;
-    auto [lon2, lat2] = greedy_tsp(lon,lat);
-    std::vector<double> cols (names.size(),1);
-    cols[0]=15;
-    cols[1]=15;
-    cols[2]=15;
-    geoplot(lat2,lon2)->marker("o").marker_colors(cols);
-    text(lon,lat,names);
+    for( auto& route : solution )
+    {
+        std::vector<double> x;
+        std::vector<double> y;
+        std::vector<std::string> tag;
+
+        for( auto& city : route )
+        {
+            x.push_back( city.m_coordinate.length );
+            y.push_back( city.m_coordinate.width );
+            tag.push_back( city.m_name );
+        }
+
+        std::vector<double> cols (route.size(), color);
+        color += 25;
+        geoplot(y,x)->marker("o").marker_colors(cols);
+        text(x,y,tag);
+    }
     show();
 }
 
+std::pair<int,int> generate_cities_to_swap(Route route){
+
+    const int FIRST_CITY = 1;
+    const int LAST_CITY = 2;
+
+    int first_city = FIRST_CITY + std::rand() % ( route.size() - LAST_CITY );
+    int second_city = FIRST_CITY + std::rand() % ( route.size() - LAST_CITY );
+    while(first_city == second_city){
+        second_city = FIRST_CITY + std::rand() % ( route.size() - LAST_CITY );
+    }
+    return {first_city, second_city};
+}
+
+void swap_cities(City& first, City& second){
+    City temp = first;
+    first = second;
+    second = temp;
+}
+
+Route createNeighbor(Route route){
+    Route new_route(route);
+    std::pair<int,int> cities_to_swap = generate_cities_to_swap(route);
+    swap_cities(new_route[ cities_to_swap.first ], new_route[ cities_to_swap.second ]);
+    return new_route;
+}
+
+bool are_route_equal( Route first_route, Route second_route){
+    if( first_route.size() != second_route.size() )
+        return false;
+    for (int i=0; i < first_route.size(); i++){
+        if(first_route[i].m_name != second_route[i].m_name){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool is_already_in_neighbor(std::vector<Route> neighbors, Route candi ){
+    for(int i=0; i< neighbors.size(); i++){
+        if( are_route_equal(neighbors[i], candi) )
+            return true;
+    }
+    return false;
+}
+
+std::vector<Route> getNeighbors(Route route){
+
+    const int first_city = 1;
+    const int last_city = 1;
+    const int ignored_cities = first_city + last_city;
+    const int current_route = 1;
+    std::vector<Route> neighbors;
+    int number_actually_needed_neighbors = ( route.size() - ignored_cities > NUMBER_OF_NEIGHBORS ) ? NUMBER_OF_NEIGHBORS : route.size() - ignored_cities - current_route;
+
+    for(int i=0; i < number_actually_needed_neighbors; i++){
+        Route new_neighbor = createNeighbor(route);
+        if(!is_already_in_neighbor( neighbors, new_neighbor ))
+            neighbors.push_back( new_neighbor );
+        else
+            --i;
+    }
+    return neighbors;
+}
+
+double fitness(Route route){
+    double route_length = 0;
+    for (int i = 0; i < route.size() - 1; i++)
+        route_length += distance_between_cities( route[i], route[i + 1] );
+    return route_length;
+}
+
+Route find_best_candidate(std::vector<Route> neighbors){
+
+    Route best_candidate = neighbors[0];
+
+    for (int i = 0; i < neighbors.size(); i++){
+        // display(neighbors[i]);
+        if( !is_in_tabu_list(neighbors[i]) && fitness(neighbors[i]) <= fitness(best_candidate) ){
+            best_candidate = neighbors[i];
+        }
+    }
+    return best_candidate;
+
+}
+
+void remove_first_route_from_tabu_list(){
+    std::reverse(std::begin(tabu_list), std::end(tabu_list));
+    tabu_list.pop_back();
+    std::reverse(std::begin(tabu_list), std::end(tabu_list));
+}
+
+
+bool stopping_condition( Route& sBest, Route& prevSBest, int threshold ){
+    if( is_near_enough_to(fitness(sBest), fitness(prevSBest), 1 ) )
+        counter++;
+    else
+    {   prevSBest = sBest;
+        counter = 0;
+    }
+    // std::cout << std::setw(5) << counter << " " << fitness(prevSBest) << " " << fitness(sBest) << std::endl;
+
+    if( counter > threshold )
+        return false;
+    else
+        return true;
+}
+
+bool is_in_tabu_list(Route route){
+    for(int i=0; i < tabu_list.size(); i++){
+        if(are_route_equal(tabu_list[i], route)){
+            return true;
+        }
+    }
+    return false;
+}
+
+std::vector<Route> build_init_solution()
+{
+    std::vector<Route> solution( numb_of_vehicles, Route(1, cities[0]) );
+    for( auto& route : solution)
+    {
+        int current_load = max_vehicle_capacity;
+        while( current_load > 0 )
+        {
+            auto it_new_city = std::find_if( std::next( std::begin( cities ) ),
+                                             std::end( cities ),
+                                             [current_load]( City & c ){
+                                                 return *(c.m_visit_counter) == 0 &&
+                                                        c.m_demand_capacity < current_load;
+                                             });
+
+            if( it_new_city != std::end( cities ) )
+            {
+                (*(it_new_city->m_visit_counter))++;
+                current_load -= it_new_city->m_demand_capacity;
+                route.push_back( *it_new_city );
+            }
+            else
+                break;
+        }
+    }
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+    for( auto& route : solution)
+        std::shuffle(std::next(route.begin()), route.end(), g);
+
+    for( auto& route : solution)
+        route.push_back( cities[0] );
+
+    // for( auto& route : solution)
+    // {
+    //     for( auto& city : route )
+    //         std::cout << city.m_name << " ";
+    //     std::cout << std::endl;
+    // }
+    return solution;
+}
+
+Route tsp_tabu_search( Route s0 ){
+    Route sBest {s0};
+    Route bestCandidate {s0};
+    tabu_list = {};
+    tabu_list.push_back(s0);
+    Route prevSBest{ sBest };
+
+    while( stopping_condition( sBest, prevSBest, 1000 ) ){
+        std::vector<Route> neighbors = getNeighbors(bestCandidate);
+        bestCandidate = find_best_candidate(neighbors);
+
+        if(fitness(bestCandidate) < fitness(sBest)){
+            sBest = bestCandidate;
+        }
+        tabu_list.push_back(bestCandidate);
+
+        if(tabu_list.size() > max_tabu_size){
+            remove_first_route_from_tabu_list();
+        }
+    }
+        display(sBest);
+    return sBest;
+}
+
+
+// ========================================= MAIN ==================================
 int main() {
-    // distance_for_two_cities_with_coordinates_from_pythagorean_triangle();
-    // is_near_enough_to_test();
-    // distance_for_two_cities();
-    // distance_matrix_for_two_cities();
-    // make_real_distance_matrix();
-    
-    draw_cities();
+    std::srand(time(0));
+    auto solution = build_init_solution();
+    // draw_solution( solution, 5 );
+    for( auto& route : solution )
+    {
+        counter = 0;
+        route = tsp_tabu_search( route );
+    }
+    draw_solution( solution, 25 );
     return 0;
 }
--- cvrp_tabu_search.hpp
+++ cvrp_tabu_search.hpp
@@ -1,20 +1,27 @@
 #ifndef CVRP_TABU_SEARCH_GUARD
 #define CVRP_TABU_SEARCH_GUARD
 
-#include <tuple>
-#include <cstdlib>
-#include <random>
-#include <iostream>
 #include <cassert>
-#include <vector>
 #include <cmath>
+#include <cstdlib>
+#include <memory>
+#include <iostream>
 #include <iomanip>
 #include <thread>
+#include <tuple>
+#include <random>
+#include <vector>
 #include <matplot/matplot.h>
+#include <chrono>
+#include <thread>
+
+#define NUMBER_OF_NEIGHBORS 10
 
 //potrzeba dokładnych danych
 const double geo_degree_in_km = 111.1; 
-
+const int numb_of_vehicles = 5;
+const int max_vehicle_capacity = 1000;
+const int max_tabu_size = 25;
 //typ dla miasta szerokosc/długosc geo
 struct Coordinate{
     double width;
@@ -23,55 +30,65 @@ struct Coordinate{
 
 struct City{
     Coordinate m_coordinate;
-    std::string name;
+    std::string m_name;
+    int m_demand_capacity;
+    std::shared_ptr<int> m_visit_counter;
 };
 
-std::vector<std::string> name_of_cities { "Krakow", "Bialystok", "Bie-Biala", "Chrzanow", "Gdansk", "Gdynia", "Gliwice", "Gromnik", "Katowice", "Kielce", "Krosno", "Krynica", "Lublin", "Lodz", "Malbork", "Nowy Targ", "Olsztyn", "Poznan", "Pulawy", "Radom", "Rzeszow", "Sandomierz", "Szczecin", "Szczucin", "Szk Poreba", "Tarnow", "Warszawa", "Wieliczka", "Wroclaw", "Zakopane", "Zamosc" };
-std::vector<Coordinate> cities = {
-        {50.0619474, 19.9368564},
-        {53.132398, 23.1591679},
-        {49.822118, 19.0448936},
-        {50.1411926, 19.4028502},
-        {54.347629, 18.6452324},
-        {54.5164982, 18.5402738},
-        {50.294113, 18.6657306},
-        {53.7361111, 15.9897222},
-        {50.2598987, 19.0215852},
-        {50.8716657, 20.6314728},
-        {49.6938045, 21.7651458},
-        {53.0336093, 23.5880673},
-        {51.250559, 22.5701022},
-        {51.7687323, 19.4569911},
-        {54.0359361, 19.0348817},
-        {49.4818229, 20.030541},
-        {53.7766839, 20.476507},
-        {52.4082663, 16.9335199},
-        {51.4171713, 21.9720582},
-        {51.4022557, 21.1541546},
-        {50.0374531, 22.0047174},
-        {50.6793066, 21.7495055},
-        {53.4301818, 14.5509623},
-        {50.3090256, 21.075161},
-        {50.8275855, 15.5211659},
-        {50.0123784, 20.9880739},
-        {52.2319581, 21.0067249},
-        {49.9823773, 20.0602114},
-        {51.1089776, 17.0326689},
-        {49.2969446, 19.950659},
-        {50.7170854, 23.2525711}
-    }; 
+using Route = std::vector<City>;
+using Tabu = std::vector<Route>;
 
-double distance_between_cities(Coordinate first_city, Coordinate second_city);
-void distance_for_two_cities_with_coordinates_from_pythagorean_triangle();
+Tabu tabu_list {};
+int counter{0};
+
+std::vector<City> cities = {
+    { {50.0619474, 19.9368564}, "Krakow", 0, std::make_shared<int>(0) },
+    { {53.132398,  23.1591679}, "Bialystok", 500, std::make_shared<int>(0) },
+    { {49.822118,  19.0448936}, "Bie-Biala", 50, std::make_shared<int>(0) },
+    { {50.1411926, 19.4028502}, "Chrzanow", 400, std::make_shared<int>(0) },
+    { {54.347629,  18.6452324}, "Gdansk", 200, std::make_shared<int>(0) },
+    { {54.5164982, 18.5402738}, "Gdynia", 100, std::make_shared<int>(0) },
+    { {50.294113,  18.6657306}, "Gliwice", 40, std::make_shared<int>(0) },
+    { {53.7361111, 15.9897222}, "Gromnik", 200, std::make_shared<int>(0) },
+    { {50.2598987, 19.0215852}, "Katowice", 300, std::make_shared<int>(0) },
+    { {50.8716657, 20.6314728}, "Kielce", 30, std::make_shared<int>(0) },
+    { {49.6938045, 21.7651458}, "Krosno", 60, std::make_shared<int>(0) },
+    { {53.0336093, 23.5880673}, "Krynica", 50, std::make_shared<int>(0) },
+    { {51.250559,  22.5701022}, "Lublin", 60, std::make_shared<int>(0) },
+    { {51.7687323, 19.4569911}, "Lodz", 160, std::make_shared<int>(0) },
+    { {54.0359361, 19.0348817}, "Malbork", 100, std::make_shared<int>(0) },
+    { {49.4818229, 20.0305410}, "Nowy Targ", 120, std::make_shared<int>(0) },
+    { {53.7766839, 20.4765070}, "Olsztyn", 300, std::make_shared<int>(0) },
+    { {52.4082663, 16.9335199}, "Poznan", 100, std::make_shared<int>(0) },
+    { {51.4171713, 21.9720582}, "Pulawy", 200, std::make_shared<int>(0) },
+    { {51.4022557, 21.1541546}, "Radom", 100, std::make_shared<int>(0) },
+    { {50.0374531, 22.0047174}, "Rzeszow", 60, std::make_shared<int>(0) },
+    { {50.6793066, 21.7495055}, "Sandomierz", 200, std::make_shared<int>(0) },
+    { {53.4301818, 14.5509623}, "Szczecin", 150, std::make_shared<int>(0) },
+    { {50.3090256, 21.0751610}, "Szczucin", 60, std::make_shared<int>(0) },
+    { {50.8275855, 15.5211659}, "Szk Poreba", 50, std::make_shared<int>(0) },
+    { {50.0123784, 20.9880739}, "Tarnow", 70, std::make_shared<int>(0) },
+    { {52.2319581, 21.0067249}, "Warszawa", 200, std::make_shared<int>(0) },
+    { {49.9823773, 20.0602114}, "Wieliczka", 90, std::make_shared<int>(0) },
+    { {51.1089776, 17.0326689}, "Wroclaw", 40, std::make_shared<int>(0) },
+    { {49.2969446, 19.9506590}, "Zakopane", 200, std::make_shared<int>(0) },
+    { {50.7170854, 23.2525711}, "Zamosc", 30, std::make_shared<int>(0) }
+};
+
+double distance_between_cities( City& first_city, City& second_city);
 bool is_near_enough_to(double supect, double center, double measurement_error = 0.000001);
 void distance_for_two_cities();
-void distance_matrix_for_two_cities();
-void make_real_distance_matrix();
-void draw_cities();
+std::vector<Route> build_init_solution();
+void draw_solution(std::vector<Route> solution);
+void display(Route route);
 
-std::tuple<std::vector<double>,
-           std::vector<double>,
-           std::vector<std::string>> our_cities();
+std::pair<int,int> generate_cities_to_swap(Route route);
+void swap_cities(City& first, City& second);
+Route createNeighbor(Route route);
+bool are_route_equal(Route first_route, Route second_route);
+bool is_already_in_neighbor(std::vector<Route> neighbors, Route candi );
+std::vector<Route> getNeighbors(Route route);
+bool is_in_tabu_list(Route route);
+double fitness(Route route);
 
-void is_near_enough_to_test();
 #endif //CVRP_TABU_SEARCH_GUARD
